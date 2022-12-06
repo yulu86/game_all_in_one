15c6017595d017df99917d0c39cb8042d4ba1fe0
Yulu Xu <xuyulu86@126.com>
2022-12-07 00:13:06 +0800

    refactor sprite

--- lib/src/game_controller.dart
+++ lib/src/game_controller.dart
@@ -1,1 +1,5 @@
+/// 精灵图
 const marioGameImage = 'mario_sprites.gif';
+
+/// 单元大小
+const unitSize = 32.0;
--- lib/src/mario_game.dart
+++ lib/src/mario_game.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:game_all_in_one/src/actors/goomba.dart';
@@ -11,9 +12,11 @@ class MarioGame extends FlameGame {
   @override
   Color backgroundColor() => const Color(0xFF7B93F1);
 
+  late Image gameSpriteImage;
+
   @override
   Future<void>? onLoad() async {
-    await Flame.images.load(marioGameImage);
+    gameSpriteImage = await Flame.images.load(marioGameImage);
 
     for (int i = 0; i < 100; i++) {
       add(GroundBrick(
@@ -22,5 +25,35 @@ class MarioGame extends FlameGame {
     }
     add(Goomba());
     add(Mario());
+  }
+
+  Sprite getSprite({
+    required Vector2 size,
+    required Vector2 position,
+  }) {
+    return Sprite(
+      gameSpriteImage,
+      srcSize: size,
+      srcPosition: position,
+    );
+  }
+
+  /// 获取精灵动画
+  SpriteAnimation getAnimation({
+    required Vector2 size,
+    required List<Vector2> frames,
+    double stepTime = double.infinity,
+  }) {
+    final sprites = frames
+        .map((srcPosition) => Sprite(
+              gameSpriteImage,
+              srcSize: size,
+              srcPosition: srcPosition,
+            ))
+        .toList();
+    return SpriteAnimation.spriteList(
+      sprites,
+      stepTime: stepTime,
+    );
   }
 }
