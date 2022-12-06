8210a0db9a320cee7dbe7a7d72ce511f156e5008
Yulu Xu <xuyulu86@126.com>
2022-12-06 22:58:47 +0800

    add goomba

--- lib/src/mario_game.dart
+++ lib/src/mario_game.dart
@@ -1,8 +1,17 @@
 import 'dart:ui';
 
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:game_all_in_one/src/actors/goomba.dart';
 
 class MarioGame extends FlameGame {
   @override
   Color backgroundColor() => const Color(0xFF7B93F1);
+
+  @override
+  Future<void>? onLoad() async {
+    await Flame.images.load('mario_sprites.gif');
+
+    add(Goomba());
+  }
 }
