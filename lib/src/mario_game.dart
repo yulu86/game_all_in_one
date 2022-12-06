d78efbdf8b25ea5384babb86abb9255adb2ab49c
Yulu Xu <xuyulu86@126.com>
2022-12-07 00:36:03 +0800

    refactor sprite

--- lib/src/mario_game.dart
+++ lib/src/mario_game.dart
@@ -6,6 +6,7 @@ import 'package:game_all_in_one/src/actors/goomba.dart';
 import 'package:game_all_in_one/src/actors/mario.dart';
 import 'package:game_all_in_one/src/game_controller.dart';
 import 'package:game_all_in_one/src/objects/ground_brick.dart';
+import 'package:game_all_in_one/src/objects/little_grass.dart';
 
 class MarioGame extends FlameGame {
   @override
@@ -17,6 +18,7 @@ class MarioGame extends FlameGame {
   Future<void>? onLoad() async {
     gameSpriteImage = await Flame.images.load(marioGameImage);
 
+    add(LittleGrass());
     for (int i = 0; i < 100; i++) {
       add(GroundBrick(
         position: Vector2(32.0 * i, 182.0),
---
+++ lib/src/objects/little_grass.dart
@@ -0,0 +1,17 @@
+import 'package:flame/components.dart';
+import 'package:game_all_in_one/src/game_controller.dart';
+import 'package:game_all_in_one/src/mario_game.dart';
+
+class LittleGrass extends SpriteComponent with HasGameRef<MarioGame> {
+  LittleGrass() : super(size: Vector2(96, 38), anchor: Anchor.bottomLeft);
+
+  @override
+  Future<void>? onLoad() async {
+    sprite = getSprite(
+      image: game.gameSpriteImage,
+      size: Vector2(48, 19),
+      position: Vector2(48, 176),
+    );
+    position = Vector2(160, 182);
+  }
+}
