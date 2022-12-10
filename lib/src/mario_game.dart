544c75b6323eae9e1815863101cc98672f004f9c
Yulu Xu <xuyulu86@126.com>
2022-12-10 21:53:29 +0800

    增加灌木丛

--- lib/src/mario_game.dart
+++ lib/src/mario_game.dart
@@ -5,6 +5,7 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:game_all_in_one/src/actors/enemy/goomba.dart';
 import 'package:game_all_in_one/src/actors/player/mario.dart';
+import 'package:game_all_in_one/src/objects/background/bush.dart';
 import 'package:game_all_in_one/src/objects/background/cloud.dart';
 import 'package:game_all_in_one/src/objects/obstacle/ground_brick.dart';
 import 'package:game_all_in_one/src/objects/background/little_grass.dart';
@@ -34,6 +35,7 @@ class MarioGame extends FlameGame with HasKeyboardHandlerComponents {
         ),
       ]);
     }
+    add(Bush(position: Vector2(30, size.y - 64)));
     add(Cloud(position: Vector2(160, 350)));
     add(LittleGrass(position: Vector2(160, size.y - 64)));
     add(Pipe(position: Vector2(320, size.y - 64)));
---
+++ lib/src/objects/background/bush.dart
@@ -0,0 +1,37 @@
+import 'package:flame/components.dart';
+import 'package:game_all_in_one/src/objects/background/object_size.dart';
+import 'package:game_all_in_one/src/objects/game_object.dart';
+
+/// 灌木丛
+class Bush extends GameObject {
+  Bush({
+    required Vector2 position,
+    ObjectSize objectSize = ObjectSize.small,
+  }) : super(
+          position: position,
+          srcSize: _getSrcSize(objectSize),
+          srcPosition: _getSrcPosition(objectSize),
+        );
+
+  static Vector2 _getSrcSize(ObjectSize objectSize) {
+    switch (objectSize) {
+      case ObjectSize.large:
+        return Vector2(64, 16);
+      case ObjectSize.medium:
+        return Vector2(48, 16);
+      case ObjectSize.small:
+        return Vector2(32, 16);
+    }
+  }
+
+  static Vector2 _getSrcPosition(ObjectSize objectSize) {
+    switch (objectSize) {
+      case ObjectSize.large:
+        return Vector2(85, 253);
+      case ObjectSize.medium:
+        return Vector2(151, 253);
+      case ObjectSize.small:
+        return Vector2(51, 253);
+    }
+  }
+}
