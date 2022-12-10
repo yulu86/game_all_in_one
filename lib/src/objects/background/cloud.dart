aeb5e0a554d5f2dfb06bd49776539810732050be
Yulu Xu <xuyulu86@126.com>
2022-12-10 22:06:52 +0800

    增加云朵

--- lib/src/objects/background/cloud.dart
+++ lib/src/objects/background/cloud.dart
@@ -1,11 +1,37 @@
 import 'package:flame/components.dart';
+import 'package:game_all_in_one/src/objects/background/object_size.dart';
 import 'package:game_all_in_one/src/objects/game_object.dart';
 
+/// 云朵
 class Cloud extends GameObject {
-  Cloud({required Vector2 position})
-      : super(
+  Cloud({
+    required Vector2 position,
+    ObjectSize objectSize = ObjectSize.small,
+  }) : super(
           position: position,
-          srcSize: Vector2(48, 24),
-          srcPosition: Vector2(46, 198),
+          srcSize: _getSrcSize(objectSize),
+          srcPosition: _getSrcPosition(objectSize),
         );
+
+  static Vector2 _getSrcSize(ObjectSize objectSize) {
+    switch (objectSize) {
+      case ObjectSize.large:
+        return Vector2(64, 24);
+      case ObjectSize.medium:
+        return Vector2(48, 24);
+      case ObjectSize.small:
+        return Vector2(32, 24);
+    }
+  }
+
+  static Vector2 _getSrcPosition(ObjectSize objectSize) {
+    switch (objectSize) {
+      case ObjectSize.large:
+        return Vector2(96, 198);
+      case ObjectSize.medium:
+        return Vector2(46, 198);
+      case ObjectSize.small:
+        return Vector2(162, 198);
+    }
+  }
 }
