2f1e69246368b523c85c45c88f5ca82c53a292aa
Yulu Xu <xuyulu86@126.com>
2022-12-11 00:39:59 +0800

    抽取segment

--- lib/src/segment/segment_manager.dart
+++ lib/src/segment/segment_manager.dart
@@ -9,18 +9,22 @@ final segments = [
 
 final segment0 = Segment(
   index: 1,
-  blocks: [
-    SegmentBlock(gridPosition: Vector2(0, 0), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(0, 1), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(1, 0), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(1, 1), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(2, 0), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(2, 1), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(3, 0), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(3, 1), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(4, 0), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(4, 1), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(5, 0), blockType: GroundBrick),
-    SegmentBlock(gridPosition: Vector2(5, 1), blockType: GroundBrick),
-  ],
+  blocks: _buildGroundBricks(17),
 );
+
+List<SegmentBlock> _buildGroundBricks(int colSize) {
+  List<SegmentBlock> result = [];
+  result.addAll(_buildGroundBrick(colSize, 0));
+  result.addAll(_buildGroundBrick(colSize, 1));
+  return result;
+}
+
+List<SegmentBlock> _buildGroundBrick(int colSize, int row) {
+  return List.generate(
+    colSize,
+    (index) => SegmentBlock(
+      gridPosition: Vector2(index as double, row as double),
+      blockType: GroundBrick,
+    ),
+  ).toList();
+}
